6278d6ceafe81793af1dee01261570cde4b972d1
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2019-05-19 19:02:41 +0300

    Update TrapData.cs

--- GL.Servers/GL.Servers.CoC/Files/CSV_Logic/Logic/TrapData.cs
+++ GL.Servers/GL.Servers.CoC/Files/CSV_Logic/Logic/TrapData.cs
@@ -21,7 +21,7 @@ namespace GL.Servers.CoC.Files.CSV_Logic.Logic
 
         internal override void LoadingFinished()
         {
-            this.BuildResourceData = (BuildingClassData) CSV.Tables.Get(Gamefile.BuildingClass).GetData(this.BuildResource);
+            this.BuildResourceData = (ResourceData) CSV.Tables.Get(Gamefile.BuildingClass).GetData(this.BuildResource);
 
             if (this.BuildResourceData == null)
             {
